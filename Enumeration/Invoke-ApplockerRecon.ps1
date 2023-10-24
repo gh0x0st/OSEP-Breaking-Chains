ffae64e2bff5a25cb523f54d6f09b6aaa8ca7b55
Tristram <47216435+gh0x0st@users.noreply.github.com>
2023-10-24 10:31:09 -0400

    Update Invoke-ApplockerRecon.ps1

--- Enumeration/Invoke-ApplockerRecon.ps1
+++ Enumeration/Invoke-ApplockerRecon.ps1
@@ -32,20 +32,24 @@ function Invoke-ApplockerRecon {
     } 
 
     # Parse through available rules
-    $Rules = Get-ChildItem -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SrpV2"
-
-    foreach ($ID in $Rules) {
-        $RuleName = $($ID.PSChildName)
-        switch ($(Get-ItemProperty -Path "$($ID.PSPath)" -Name "EnforcementMode" -ErrorAction SilentlyContinue).EnforcementMode) {
-            1 { $Enforcement = 'Enforced' }
-            0 { $Enforcement = 'Not Enforced' }
-            $Null { $Enforcement = 'Not Configured' }
-        }
+    $Rules = Get-ChildItem -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SrpV2" -ErrorAction 0
+    If ($Rules) {
+        foreach ($ID in $Rules) {
+            $RuleName = $($ID.PSChildName)
+            switch ($(Get-ItemProperty -Path "$($ID.PSPath)" -Name "EnforcementMode" -ErrorAction SilentlyContinue).EnforcementMode) {
+                1 { $Enforcement = 'Enforced' }
+                0 { $Enforcement = 'Not Enforced' }
+                $Null { $Enforcement = 'Not Configured' }
+            }
     
-        $Actions = Get-ChildItem -Path "$($ID.PSPath)" -ErrorAction SilentlyContinue
-        foreach ($Action in $Actions) {
-            $XML = Get-ItemProperty -Path "$($Action.PSPath)" -Name "Value" -ErrorAction SilentlyContinue
-            Parse-XML -XmlString $XML.Value | Select @{l = 'Rule'; e = { $RuleName } }, @{l = 'Enforcement'; e = { $Enforcement } }, ID, Name, Description, UserOrGroupSid, Action, @{l = 'Conditions'; e = { $_.Conditions.FilePathCondition.Path } }
+            $Actions = Get-ChildItem -Path "$($ID.PSPath)" -ErrorAction SilentlyContinue
+            foreach ($Action in $Actions) {
+                $XML = Get-ItemProperty -Path "$($Action.PSPath)" -Name "Value" -ErrorAction SilentlyContinue
+                Parse-XML -XmlString $XML.Value | Select @{l = 'Rule'; e = { $RuleName } }, @{l = 'Enforcement'; e = { $Enforcement } }, ID, Name, Description, UserOrGroupSid, Action, @{l = 'Conditions'; e = { $_.Conditions.FilePathCondition.Path } }
+            }
         }
+    }
+    Else {
+        Write-Output 'Applocker is not configured'
     }
 }
